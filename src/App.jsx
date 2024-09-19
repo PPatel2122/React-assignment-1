d498fac85fce1b06e488b1ef479292103c5d30e7
PAWANPATEL <pp467354@gmail.com>
2024-09-19 15:23:16 +0530

    added my project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+import Footer from './Footer'
+import Header from './Header'
+import Icon from './Icon'
+import Navbar from './Navbar'
+import Section from './Section'
+
+
+
+const App = () => {
+  
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <Icon/>
+    <Section/>
+    <Footer/>
+      
+    </>
+  )
+}
+
+export default App
